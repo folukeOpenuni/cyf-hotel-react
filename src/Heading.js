8e2d33bc300763f018fb22549b72968447fcae89
folukeOpenuni <folukeagbede@googlemail.com>
2019-05-22 12:43:04 +0100

    created heading component

---
+++ src/Heading.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img
+        className="App-logo"
+        src={
+          "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BBL1H4v.img?h=468&w=624&m=6&q=60&o=f&l=f&x=560&y=349"
+        }
+      />
+      CYF Hotel
+    </header>
+  );
+};
+
+export default Heading;
